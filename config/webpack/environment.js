832732241178b7ca1115557c56567ba00e30e84f
Néville Dubuis <ndubuis@gmail.com>
2020-12-02 15:57:12 +0100

    add angular and htlm loader to webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
-
+const typescript =  require('./loaders/typescript')
+environment.loaders.append('html', {
+  test: /\.html$/,
+  use:[
+    { loader: 'html-loader' }
+  ]
+})
+environment.loaders.prepend('typescript', typescript)
 module.exports = environment
