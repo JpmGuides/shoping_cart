d78874b637746442179fd020ce0366c3742fbc96
Néville Dubuis <ndubuis@gmail.com>
2020-12-14 16:41:41 +0100

    add date picker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,12 @@ require("bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on("DOMNodeInserted", function(e) {
+  if ($(e.target).find('.datepicker')) {
+    console.log('hello')
+    $(e.target).find('.datepicker').datepicker({
+      uiLibrary: 'bootstrap4'
+    });
+  }
+});
