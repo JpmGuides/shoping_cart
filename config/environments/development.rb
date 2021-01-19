58577883242f1054b1122981b3e96b147dbb5d5e
Néville Dubuis <ndubuis@gmail.com>
2021-01-19 15:56:47 +0100

    remove httplog

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,8 +63,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  HttpLog.configure do |config|
-    config.logger = Rails.logger
-  end
 end
